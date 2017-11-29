1440110d33da36dfe70c816d4e2760293b2c7f93
Minh VU <ngoc.vu@polytechnique.org>
2017-11-29 11:21:46 +0000

    fix Udacity code

--- l02/ud851-Exercises/Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ l02/ud851-Exercises/Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -49,15 +50,23 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenAddressButton(View v) {
         // TODO (5) Store an address in a String
-        String googleHq = "1600 Amphitheatre Parkway, CA";
+        String googleHq = "1600 Amphitheatre Parkway, Mountain+View, California";
 
         // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
         Uri.Builder builder = new Uri.Builder();
         builder.scheme("geo")
                 .path("0,0")
-                .query(googleHq);
+                .appendQueryParameter("q", googleHq);
+        // the example in Udacity is wrong
+        // when using .query it only add the query after ?
+        // for example uri will be geo:/0%2C0?1600%20Amphitheatre%20Parkway%2C%20Mountain%2BView%2C%20California
+        // however we need to have
+        //          geo:/0%2C0?q=1600%20Amphitheatre%20Parkway%2C%20Mountain%2BView%2C%20California
+        // the solution is to use appendQueryParameter
 
         Uri uri = builder.build();
+//        Uri uri = Uri.parse("geo:0,0?q=1600 Amphitheatre Parkway, Mountain View, California");
+        Log.v("mapIntent", "uri = " + uri.toString());
 
         // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
         showMap(uri);
