73013d103f8c2897416812a5b685ec92965b73de
Minh VU <ngoc.vu@polytechnique.org>
2017-12-23 09:03:32 +0000

    wip lesson 9

--- l02/ud851-Exercises/Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ l02/ud851-Exercises/Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -27,17 +27,17 @@ import android.support.annotation.NonNull;
 // Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
 
-    // TODO (1) Define final integer constants for the directory of tasks and a single item.
+    // DONE (1) Define final integer constants for the directory of tasks and a single item.
     // It's convention to use 100, 200, 300, etc for directories,
     // and related ints (101, 102, ..) for items in that directory.
     public static final int TASKS = 100;
     public static final int TASK_WITH_ID = 101;
 
-    // TODO (3) Declare a static variable for the Uri matcher that you construct
+    // DONE (3) Declare a static variable for the Uri matcher that you construct
     public static final UriMatcher sURI_MATCHER = buildUriMatcher();
 
 
-    // TODO (2) Define a static buildUriMatcher method that associates URI's with their int match
+    // DONE (2) Define a static buildUriMatcher method that associates URI's with their int match
     public static UriMatcher buildUriMatcher() {
         UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
         uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASKS);
--- l02/ud851-Exercises/Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ l02/ud851-Exercises/Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -17,10 +17,12 @@
 package com.example.android.todolist.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
@@ -78,16 +80,31 @@ public class TaskContentProvider extends ContentProvider {
 
     @Override
     public Uri insert(@NonNull Uri uri, ContentValues values) {
-        // TODO (1) Get access to the task database (to write new data to)
+        // DONE (1) Get access to the task database (to write new data to)
+        final SQLiteDatabase sqLiteDatabase = mTaskDbHelper.getWritableDatabase();
 
-        // TODO (2) Write URI matching code to identify the match for the tasks directory
+        // DONE (2) Write URI matching code to identify the match for the tasks directory
+        int match = sUriMatcher.match(uri);
 
-        // TODO (3) Insert new values into the database
-        // TODO (4) Set the value for the returnedUri and write the default case for unknown URI's
-
-        // TODO (5) Notify the resolver if the uri has been changed, and return the newly inserted URI
-
-        throw new UnsupportedOperationException("Not yet implemented");
+        // DONE (3) Insert new values into the database
+        // DONE (4) Set the value for the returnedUri and write the default case for unknown URI's
+        Uri returnUri;
+        switch (match) {
+            case TASKS:
+                long id = sqLiteDatabase.insert(TaskContract.TaskEntry.TABLE_NAME, null, values);
+                if (id > 0) {
+                    returnUri = ContentUris.withAppendedId(TaskContract.TaskEntry.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Uknown uri: " + uri);
+        }
+        // DONE (5) Notify the resolver if the uri has been changed, and return the newly inserted URI
+        // this is to update any UI related to this database
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
     }
 
 
